056d5d009a50b13cc48680e67b549999c5693482
gksmf0160 <gksmf0160@gmail.com>
2025-07-29 13:30:48 +0900

    업데이트
    스냅킷과 포켓몬 번역파일 만듬

---
+++ poketmon-Encyclopedia/poketmon-Encyclopedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  poketmon-Encyclopedia
+//
+//  Created by 송명균 on 7/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
